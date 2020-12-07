7e34f447aebefc10d4c93a12d7e18f077a70fe8d
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-06 22:36:23 -0500

    TRIPLE WHOOPS

--- attendance/submit_form.py
+++ attendance/submit_form.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 
 
 # submit google form
-async def submit(driver):
+def submit(driver):
     loaded = EC.presence_of_element_located((By.CLASS_NAME, 'freebirdFormviewerViewNavigationSubmitButton'))
     WebDriverWait(driver, 100).until(loaded)
     submit_button = driver.find_element_by_class_name('freebirdFormviewerViewNavigationSubmitButton')
-    await submit_button.click()
+    submit_button.click()
 
